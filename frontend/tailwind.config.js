0db46878c5461c7575e757806a50b19a7643ce88
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-09-19 17:14:29 +0800

    Fix theme provider

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class', // Enable class-based dark mode
+  darkMode: 'class',
   theme: {
     extend: {},
   },
